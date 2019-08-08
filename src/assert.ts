779d874d984b736d052879bc4e393d2921162223
bagaking <zfaustk@qq.com>
2019-08-08 17:53:16 +0800

    feat (assert): add some deprecated api

--- src/assert.ts
+++ src/assert.ts
@@ -58,4 +58,48 @@ export class CAssert {
     cNotStrictEqual<T>(a: T, b: T, code: number, msg: string | Error | StringMethod) {
         return this.cok(a !== b, code, msg);
     }
+
+    /**
+     * @deprecated
+     */
+    ok<T>(condition: T, msg: string | Error | StringMethod) {
+        if (condition instanceof Promise) {
+            throw new Error("assert condition cannot be a promise");
+        }
+
+        if (condition) {
+            return;
+        }
+
+        let msgStr: string = "";
+        if (typeof msg === "string") {
+            msgStr = msg;
+            msg = new Error(msgStr);
+        } else if (msg instanceof CError) {
+            const err = (msg as CError);
+            msgStr = err.code + ": " + err.message + " stack: " + err.stack;
+        } else if (msg instanceof Error) {
+            const err = (msg as Error);
+            msgStr = err.message + " stack: " + err.stack;
+        } else {
+            msgStr = (msg as StringMethod)();
+            msg = new Error(msgStr);
+        }
+        this.log(msgStr);
+        throw msg;
+    }
+
+    /**
+     * @deprecated
+     */
+    sEqual<T>(a: T, b: T, msg: string | Error | StringMethod) {
+        return this.ok(a === b, msg);
+    }
+
+    /**
+     * @deprecated
+     */
+    sNotEqual<T>(a: T, b: T, msg: string | Error | StringMethod) {
+        return this.ok(a !== b, msg);
+    }
 }
